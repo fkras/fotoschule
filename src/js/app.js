d54aebbcf67f40b8211c19207c3af41f3e89c3eb
Fitim X Kransiqi <fitimkrasniqi@odysseyprime.nl>
2022-03-07 19:58:31 +0100

    added menu

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,18 @@ require('../scss/style.scss');
 
 /********** Paste your code here! ************/
 
-$(window).on("load",()=>{
+$(window).on("load", () => {
   $("body").addClass("hasloaded")
+  setTimeout(() => {
+    $('.banner-content').addClass('active')
+  }, 1500)
+})
+$("#menu-button").on("click touch", () => {
+  if ($("header").hasClass("menu-open")) {
+    $("header").removeClass("menu-open")
+  }
+  else {
+    $("header").addClass("menu-open")
+  }
+
 })
